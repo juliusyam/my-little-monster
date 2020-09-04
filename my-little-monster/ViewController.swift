7b914a30067e1dc99149f74b2df889033c560c92
juliusyam <jyyam1999@gmail.com>
2020-09-04 13:03:51 +0100

    Initial Commit

---
+++ my-little-monster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-little-monster
+//
+//  Created by Julius on 04/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
